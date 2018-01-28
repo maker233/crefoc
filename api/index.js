54071f23d3b653339827850750b937935ab957d3
maker233 <javiermaker@gmail.com>
2018-01-28 03:54:06 +0100

    fixing nodemon & other

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 'use strict'
 
 var mongoose = require('mongoose')
+var app = require('./app')
+var port = 3800
 
+// Conexión DDBB
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost:27017/creativefocus') //, {useMongoClient: true}) // conexion a base de datos
     .then(() => {
-      console.log('La conexión a la base de datos se ha realizado correctamente')
+      console.log('La conexión a la base de datos creativefocus se ha realizado correctamente')
+
+      // crear servidor
+      app.listen(port, () => {
+        console.log('Servidor corriendo en http://localhost:3800')
+      })
     })
     .catch(err => console.log(err))
